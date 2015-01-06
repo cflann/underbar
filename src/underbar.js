175baf8c460fc623aa5d0eb2e2a686a4b1dc04b1
Conor Flannigan <cflanni1@jhu.edu>
2015-01-05 20:33:48 -0800

    Implement 'memoize'

--- src/underbar.js
+++ src/underbar.js
@@ -300,6 +300,15 @@
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+    var alreadyCalled = {};
+
+    return function() {
+      var key = Array.prototype.slice.call(arguments);
+      if (!alreadyCalled.hasOwnProperty(key)) {
+        alreadyCalled[key] = func.apply(this, arguments);
+      }
+      return alreadyCalled[key];
+    };
   };
 
   // Delays a function for the given number of milliseconds, and then calls
